22de7a22c1ad6c84a84683204bdf064c6ce5609b
CloudStoned <cloudneo0719@gmail.com>
2024-05-21 08:13:31 +0800

    Update: added models & scanner

--- ml_model.py
+++ ml_model.py
@@ -11,3 +11,10 @@ class SvmModel:
         # Make predictions using the loaded SVM model
         prediction = self.model.predict(input_features)
         return prediction
+
+class LrModel:
+    def __init__(self):
+        self.model_dir = os.path.join(os.path.dirname(__file__), 'models')
+        self.model_path = os.path.join(self.model_dir, 'logistic_regression_classifier.joblib')
+        self.model = joblib.load(self.model_path)
+
